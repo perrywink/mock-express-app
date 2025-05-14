4c1ac8f5fd2479623e9d942b8a46d4cda1edd3ea
perrywink <isaacyjc@gmail.com>
2025-05-14 10:56:09 +1000

    staging

--- app.js
+++ app.js
@@ -7,12 +7,19 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.use(
+//   cyberchief_bolt(
+//     {
+//         key: "bolt.i3SMnhMJIMIw43A5nd72KxcpIbYBCIeVk2OZb08j",
+//         host: "http://bolt-staging-719932934.us-east-2.elb.amazonaws.com:8081",
+//     })); 
+
 app.use(
   cyberchief_bolt(
-    {
-        key: "bolt.i3SMnhMJIMIw43A5nd72KxcpIbYBCIeVk2OZb08j",
-        host: "http://bolt-staging-719932934.us-east-2.elb.amazonaws.com:8081",
-    })); 
+      {
+          key: "bolt.y75STUQbf96ZE06xBgTGOmID0iGOOhiYh63xcQ16",
+          host: "http://bolt-staging-719932934.us-east-2.elb.amazonaws.com:8081",
+      })); 
 
 // app.use(
 //   cyberchief_bolt(
